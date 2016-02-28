d6e877e1199acf3585604999cd6064501fccc5d3
Taylor LeBlanc <tleblan2@uwo.ca>
2016-02-28 18:37:40 -0500

    just updating some classes

--- src/main/java/ca/uwo/csd/cs2212/team09/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team09/App.java
@@ -1,7 +1,5 @@
 package ca.uwo.csd.cs2212.team09;
 
-import java.awt.EventQueue;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,31 +8,17 @@ public class App {
     static Logger logger = LogManager.getLogger(App.class.getName());
 
     public static void main(String[] args) {
-        System.out.println("Starting session");
+        System.out.println("Attempting to log in");
 
         //To run this: (uncanned data)
         //java -jar target/team09_FitBitProject-1.0-SNAPSHOT-jar-with-dependencies.jar
         if (args.length == 0){
-            UserInterface user = new UserInterface();
-            user.refreshData(false, "");
+            UserInterface.getData(false);
         }
         //To run this: (canned data)
         //java -jar target/team09_FitBitProject-1.0-SNAPSHOT-jar-with-dependencies.jar canned
         else if (args[0].equals("canned")) {
-            UserInterface user = new UserInterface();
-            user.refreshData(true, "");
-        }
-        else if (args[0].equals("UITEST")) {
-        	System.out.println("UITest mode.");
-        	EventQueue.invokeLater(new Runnable() {
-    			public void run() {
-    				try {
-    					MainView window = new MainView();
-    				} catch (Exception e) {
-    					e.printStackTrace();
-    				}
-    			}
-    		});
+            UserInterface.getData(true);
         }
 
         logger.trace("Exiting main");
