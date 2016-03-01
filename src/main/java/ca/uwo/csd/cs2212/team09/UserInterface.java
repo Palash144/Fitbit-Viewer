b0a174858447d59444946829d2aaf62b1bd84aff
Michael Song <songhaozhou@gmail.com>
2016-03-01 15:53:32 -0500

    Improved refreshMySummary() in UserData

--- src/main/java/ca/uwo/csd/cs2212/team09/UserInterface.java
+++ src/main/java/ca/uwo/csd/cs2212/team09/UserInterface.java
@@ -19,7 +19,7 @@ public class UserInterface {
     //TODO: Test class to test UserData refreshAll(), remove after
     public static void main(String[] args) {
         UserData testSessionData = new UserData();
-        Double[] testreceive = testSessionData.refreshAll(false, "2016-01-08");
+        double[] testreceive = testSessionData.refreshAll(false, "2016-01-08");
 
         System.out.println("testing return data:");
         for (int i = 0; i < testreceive.length; i++){
